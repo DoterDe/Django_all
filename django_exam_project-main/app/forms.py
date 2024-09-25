20ad41458d8310093170ba361a45d052a7ff1d51
Чилманов Абдуррохман Асанович <ChilmanovA@lcoast.itstep>
2024-09-25 21:52:44 +0500

    committttt

---
+++ django_exam_project-main/app/forms.py
@@ -0,0 +1,148 @@
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import UserProfile1 , Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm 
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Ваше имя', 'style': 'width: 300px; margin: 0;'})
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Фамилия', 'style': 'width: 300px; margin: 0;'})
+    )
+    avatar = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'class': 'custom-file-input'})
+    )
+    phone_number = forms.CharField(
+        max_length=17,
+        validators=[UserProfile1.phone_regex],
+        widget=forms.TextInput(attrs={'placeholder': 'Номер телефона', 'style': 'width: 300px; margin: 0;'})
+    )
+    email = forms.EmailField(       
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email','style' :'width: 300px ;margin: 0;'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Choose a username','style' : 'width: 300px ; margin: 0;'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter a strong password','style' : 'width: 300px ; margin: 0;'})
+    )
+    class Meta:
+        model = UserProfile1
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'avatar' ]  
+        # widgets = {
+        #     'username' : forms.TextInput(attrs={'class':'form-control'}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        #     'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'avatar': forms.FileInput(attrs={'class': 'form-control'}),
+            
+        # }
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        self.fields['email'].disabled = True
+
+class Register(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Ваше имя', 'style': 'width: 300px; margin: 0;'})
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Фамилия', 'style': 'width: 300px; margin: 0;'})
+    )
+    avatar = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(attrs={'class': 'custom-file-input'})
+    )
+    phone_number = forms.CharField(
+        max_length=17,
+        validators=[UserProfile1.phone_regex],
+        widget=forms.TextInput(attrs={'placeholder': 'Номер телефона', 'style': 'width: 300px; margin: 0;'})
+    )
+
+    class Meta:
+        model = UserProfile1
+        fields = ['first_name', 'last_name', 'avatar', 'phone_number']
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+        return phone_number
+
+class CodeForm(forms.Form):
+    key = forms.IntegerField(
+        label='Код подтверждения',
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Введите код',
+            'style': 'width: 300px; margin: 0;',
+            'required': 'required',
+        })
+    )
+
+class FormReg(UserCreationForm):
+    email = forms.EmailField(       
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter your email','style' :'width: 300px ;margin: 0;'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Choose a username','style' : 'width: 300px ; margin: 0;'})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Enter a strong password','style' : 'width: 300px ; margin: 0;'})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Repeat your password','style' : 'width: 300px ; margin: 0;'})
+    )
+
+    class Meta:
+        model = UserProfile1
+        fields = ['username', 'email', 'password1', 'password2']
+    def __init__(self, *args, **kwargs):
+        super(FormReg, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.label = ''
+
+# class FormLogin(AuthenticationForm):
+#     username = forms.CharField(
+#         label = '',
+#         widget=forms.TextInput(attrs={'placeholder': 'Username',
+#                                       'style' : 'width: 300px ; margin: 0;'})
+#     )
+#     password = forms.CharField(
+#         label = '',
+#         widget=forms.PasswordInput(attrs={'placeholder': 'Password',
+#                                       'style' : 'width: 300px ; margin: 0;'})
+#     )
+
+class FormLogin(AuthenticationForm):
+    username = forms.CharField(label='', max_length=30)
+    password = forms.CharField(label='', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "password")
+
+    def __init__(self, *args, **kwargs):
+        super(FormLogin, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Имя пользователя' , 'id': 'id_login_username' })
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Пароль' , 'id': 'id_login_password'})
+
+class UserForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields =  ['username', 'password' ]
+
+
+class CreatePostForm(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ['title', 'content',  'image']
+
+    def __init__(self, *args, **kwargs):
+        super(CreatePostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs['placeholder'] = 'Введите заголовок поста'
+        self.fields['content'].widget.attrs['placeholder'] = 'Введите содержание поста'
+        self.fields['image'].widget.attrs['placeholder'] = 'Выберите изображение (необязательно)'
---
+++ django_exam_project-main/app/views.py
@@ -0,0 +1,253 @@
+
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from django.shortcuts import render,redirect
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse_lazy
+from .forms import CreatePostForm , UserForm , FormLogin, FormReg , CodeForm,Register , UserProfileForm
+from .models import UserProfile1,Product,Post
+from django.views.generic.edit import CreateView, DeleteView
+from .serializers import ProductSerializers
+from django.http import JsonResponse
+from rest_framework import status, generics
+from rest_framework.viewsets import ModelViewSet,ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import authenticate
+import random
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+def generate_code():
+    random.seed()
+    return str(random.randint(10000,99999))
+
+def register_and_login_view(request):
+    signup_form = FormReg()
+    login_form = FormLogin()
+
+    if request.method == 'POST':
+        if 'signup_submit' in request.POST:
+            signup_form = FormReg(request.POST)
+            if signup_form.is_valid():
+                user = signup_form.save(commit=False)
+                user.is_active = False 
+                user.code = generate_code()  
+                user.save()  
+
+                code = user.code  
+                message = f'Ваш код подтверждения: {code}'
+                send_mail(
+                    'Код подтверждения',
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [signup_form.cleaned_data['email']],
+                    fail_silently=False
+                )
+
+                return redirect('enter_confirmation_code', user_id=user.id) 
+
+        elif 'login_submit' in request.POST:
+            login_form = FormLogin(request=request, data=request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data.get('username')
+                password = login_form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Вы успешно вошли в систему.')
+                    return redirect('home')
+                else:
+                    messages.error(request, "Аутентификация не удалась: неверное имя пользователя или пароль.")
+            else:
+                messages.error(request, "Форма входа недействительна. Пожалуйста, исправьте ошибки ниже.")
+
+    return render(request, 'authenticate/register_and_login.html', context={'signup_form': signup_form, 'login_form': login_form})
+
+def enter_confirmation_code(request, user_id):
+    if request.method == 'POST':
+        form = CodeForm(request.POST)
+        if form.is_valid():
+            code_use = form.cleaned_data.get("key")
+            try:
+                user = UserProfile1.objects.get(id=user_id)
+                if user.code == code_use:
+                    user.is_active = True 
+                    user.code = generate_code() 
+                    user.save()
+                    login(request, user)
+                    messages.success(request, 'Ваш аккаунт успешно подтверждён.')
+                    return redirect('register', user_id=user.id)
+                else:
+                    form.add_error(None, 'Неверный код подтверждения.')
+            except UserProfile1.DoesNotExist:
+                form.add_error(None, 'Пользователь не найден.')
+    else:
+        form = CodeForm()
+
+    return render(request, 'authenticate/enter_code.html', {'form': form})
+
+def register(request, user_id):
+    user = UserProfile1.objects.get(id=user_id) 
+    if request.method == 'POST':
+        form = Register(request.POST, request.FILES, instance=user)  
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация завершена успешно.')
+            return redirect('home')
+        else:
+            messages.error(request, "Ошибка при регистрации. Пожалуйста, исправьте ошибки ниже.")
+    else:
+        form = Register(instance=user)
+
+    return render(request, 'authenticate/register.html', {'form': form}) 
+
+
+@login_required
+def profile_view(request):
+    user_profile = UserProfile1.objects.get(id=request.user.id) 
+    form = UserProfileForm(instance=user_profile)  
+    return render(request, 'profile.html', {'form': form, 'profile': user_profile})
+
+@login_required
+def profile_update_view(request):
+    user_profile = UserProfile1.objects.get(id=request.user.id)
+    
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            response_data = {
+                'success': True,
+                'first_name': user_profile.first_name,
+                'email': user_profile.email,
+                'phone_number': user_profile.phone_number,
+                'avatar_url': user_profile.avatar.url
+            }
+            return JsonResponse(response_data)
+        else:
+            return JsonResponse({'success': False, 'errors': form.errors})
+
+
+@login_required
+class CreatePost(LoginRequiredMixin, CreateView):
+    model = Post 
+    success_url = reverse_lazy('home')
+    template_name = 'create_post.html'
+    form_class = CreatePostForm
+
+
+@permission_classes((IsAuthenticated,))
+class APIProductViewSet(ModelViewSet):
+    queryset =Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class APIREADProductViewSet(ReadOnlyModelViewSet):
+    queryset =Product.objects.all()
+    serializer_class = ProductSerializers
+    permission_classes = (IsAuthenticated,)
+
+# class APIProduct(generics.ListAPIView):
+#     queryset =  Product.objects.all()
+#     serializer_class = ProductSerializers
+
+# class APIProductDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset =  Product.objects.all()
+#     serializer_class = ProductSerializers
+
+
+
+# @api_view(['GET', 'POST'])
+# def api_product (request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializers(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializers(data=request.data) 
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data,status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE']) 
+# def api_product_id(request, pk): 
+#     rubric = Product.objects.get(pk=pk) 
+#     if request.method == 'GET': 
+#         serializer = ProductSerializers(rubric) 
+#         return Response(serializer.data) 
+#     elif request.method == 'PUT' or request.method == "PATCH": 
+#         serializer = ProductSerializers(rubric, data=request.data, partial = True) 
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+#     elif request.method == 'DELETE': 
+#         rubric.delete() 
+#         return Response ('Success delete',status=status.HTTP_204_NO_CONTENT)
+
+# @api_view(['GET', 'POST'])
+# def api_rubrics(request) : 
+#     if request.method == 'GET':
+#         rubrics = Product.objects.all() 
+#         serializer = RubricSerializer (rubrics, any-Irel retumn Response (serializer.data) elif request.method = 'POST': serializer = RubricSerializer (data=request .data) if serializer.is valid(): serializer.save () return Response (serializer.data, status=status.HTTP 201 CBATED return Response (serializer.errors, status-status.HITP 400 BAD REQUEST]
+
+def home(request):
+    posts=UserProfile1.objects.all()
+    if request.method == 'POST':
+        api_id = request.POST['id_product']
+        return redirect(f'api_id/{api_id}')
+
+    return render(request, 'home.html', context= {'posts':posts } )
+
+# @api_view(['GET'])
+# def api_product(request):
+#     if request.method == 'GET':
+#         product = Product.objects.all()
+#         serializer = ProductSerializers(product, many = True)
+#         return Response(serializer.data)
+# @api_view(['GET'])
+# def api_product_id(request, pk):
+#     if  request.method == 'GET':
+#         product = Product.objects.get(pk=pk)
+#         serializer = ProductSerializers(product)
+#         return Response(serializer.data)
+
+
+
+
+
+
+def email_massage(request):
+    user = UserProfile1.objects.get(id=1)
+    send_mail(
+        "Subject here",
+        "Here is the message.",
+        "from@example.com",
+        [f"{user.email}"],
+        fail_silently=False,
+    )
+    return HttpResponseRedirect("/")
+
+
+
+class DeletePost(DeleteView):
+    model = UserProfile1   
+    success_url = reverse_lazy('home')
+    template_name = 'delete.html'
+
+@login_required
+def logout_student(request):
+    logout(request)
+    return render(request, 'home.html')
